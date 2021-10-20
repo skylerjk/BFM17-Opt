4fb206153dba182d46f9027620ec828f83fe9abb
skylerjk <skke3393@colorado.edu>
2021-10-20 10:05:48 -0600

    Set CalcObjective.py to implementation for 30 simulation

--- Source/CalcObjective.py
+++ Source/CalcObjective.py
@@ -7,7 +7,10 @@ import sys
 State_Variables = ['P2l','P2c','P2n','P2p','Z5c','Z5n','Z5p','R1c','R1n','R1p','R6c','R6n','R6p','N1p','N3n','N4n','O2o']
 
 depth = 150
+# Num_Days = 360
+# Sim_Days = 1080
 Num_Days = 30
+Sim_Days = 30
 Num_SVar = len(State_Variables)
 
 # Control Flag for Initial Evaluation
@@ -27,21 +30,21 @@ NC_Tst_File_Location = 'bfm17_pom1d.nc'
 NC_Ref_Data = Dataset(NC_Ref_File_Location)
 NC_Tst_Data = Dataset(NC_Tst_File_Location)
 
-# Temp_Ref = np.zeros([Num_SVar,Num_Days,depth])
-# Temp_Tst = np.zeros([Num_SVar,num_days,depth])
+Temp_Ref = np.zeros([Num_SVar,Sim_Days,depth])
+Temp_Tst = np.zeros([Num_SVar,Sim_Days,depth])
 
-BGC_Ref_Data = np.zeros([Num_SVar,Num_Days,depth])
-BGC_Tst_Data = np.zeros([Num_SVar,Num_Days,depth])
+BGC_Ref_Data = np.zeros([Num_SVar,depth,Num_Days])
+BGC_Tst_Data = np.zeros([Num_SVar,depth,Num_Days])
 
 for i, var in enumerate(State_Variables):
-    # Temp_Ref[i,:,:] = NC_Ref_Data[var][:]
-    # BGC_Ref_Data[i,:,:] = Temp_BGC_Data[i,-360:,:].transpose()
-
-    # Temp_Tst[i,:,:] = NC_Tst_Data[var][:]
-    # BGC_Tst_Data[i,:,:] = Temp_BGC_Data[i,-360:,:].transpose()
-
-    BGC_Ref_Data[i,:,:] = NC_Ref_Data[var][:]
-    BGC_Tst_Data[i,:,:] = NC_Tst_Data[var][:]
+    Temp_Ref[i,:,:] = NC_Ref_Data[var][:]
+    # BGC_Ref_Data[i,:,:] = Temp_Ref[i,-360:,:].transpose()
+    #
+    Temp_Tst[i,:,:] = NC_Tst_Data[var][:]
+    # BGC_Tst_Data[i,:,:] = Temp_Tst[i,-360:,:].transpose()
+    #
+    BGC_Ref_Data[i,:,:] = Temp_Ref[i,:,:].transpose()
+    BGC_Tst_Data[i,:,:] = Temp_Tst[i,:,:].transpose()
 
 N = float(Num_Days*depth)
 
