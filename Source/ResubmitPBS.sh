84ded42a6f2818e31230fbc4d6596302f0a4b49e
skylerjk <skke3393@colorado.edu>
2022-03-18 11:49:44 -0600

    Adding file to resubmit the optimization run to supercomputer

---
+++ Source/ResubmitPBS.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#PBS -N BFM17-OptRun
+#PBS -A UCUB0079
+#PBS -q regular
+#PBS -l walltime=12:00:00
+#PBS -l select=01:ncpus=29:mpiprocs=32
+
+module purge
+module load gnu
+module load openmpi
+module load netcdf
+
+module load conda
+conda activate OptEnv
+
+PATH=$PATH:/glade/u/home/skylerk/dakota/bin/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$NETCDF/lib
+
+dakota -i dakota.in -o output.out -e error.err -r dakota.rst
